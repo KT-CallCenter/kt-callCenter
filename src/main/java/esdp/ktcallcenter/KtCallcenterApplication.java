0a79899f5b3448f5203ef437d21f04a12a6fb013
Timur Abuzyarov <timur.abuzyarov@gmail.com>
2020-02-27 17:21:12 +0600

    initial commit

---
+++ src/main/java/esdp/ktcallcenter/KtCallcenterApplication.java
@@ -0,0 +1,13 @@
+package esdp.ktcallcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtCallcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtCallcenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/esdp/ktcallcenter/KtCallcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package esdp.ktcallcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtCallcenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
