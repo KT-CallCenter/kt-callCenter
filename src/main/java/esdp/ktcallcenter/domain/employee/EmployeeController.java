0ef9b7de177a4542a7d74ab90630feb466ee5c82
Chingis <cmontayev@gmail.com>
2020-03-03 20:44:20 +0600

    #6 разработал контролер и ftlh шаблон добавление сотрудника

--- src/main/java/esdp/ktcallcenter/domain/employee/EmployeeController.java
+++ src/main/java/esdp/ktcallcenter/domain/employee/EmployeeController.java
@@ -1,7 +1,29 @@
 package esdp.ktcallcenter.domain.employee;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/employees")
+@AllArgsConstructor
 public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @GetMapping
+    public String addEmployee(){
+        return "addEmployee";
+    }
+
+
+    @PostMapping("/addEmployee")
+    public String addEmployee(@RequestParam String firstName,@RequestParam String lastName){
+        employeeService.addEmployee(firstName,lastName);
+        return "redirect:/employees";
+    }
+
+
 }
