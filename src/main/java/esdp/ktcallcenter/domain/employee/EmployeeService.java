229023c90acce3debf383700b82dd9fa57e08078
Chingis <cmontayev@gmail.com>
2020-03-03 20:42:51 +0600

    #6 разработал сервес добавление сотрудника

--- src/main/java/esdp/ktcallcenter/domain/employee/EmployeeService.java
+++ src/main/java/esdp/ktcallcenter/domain/employee/EmployeeService.java
@@ -1,8 +1,16 @@
 package esdp.ktcallcenter.domain.employee;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class EmployeeService {
+private final EmployeeRepository employeeRepository;
 
+
+    public void addEmployee(String firstName, String lastName) {
+        Employee employee = Employee.builder().firstName(firstName).lastName(lastName).build();
+        employeeRepository.save(employee);
+    }
 }
