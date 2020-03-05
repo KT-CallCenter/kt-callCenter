942595a60cac4dce3aefa4f8f311ea2d9da5085a
Chingis <cmontayev@gmail.com>
2020-03-05 21:59:24 +0600

    [#6] - изменил типы полей в DTO

--- src/main/java/esdp/ktcallcenter/domain/employee/EmployeeDTO.java
+++ src/main/java/esdp/ktcallcenter/domain/employee/EmployeeDTO.java
@@ -1,17 +1,15 @@
 package esdp.ktcallcenter.domain.employee;
 
+import esdp.ktcallcenter.domain.contract.Contract;
 import esdp.ktcallcenter.domain.language.Language;
 import esdp.ktcallcenter.domain.region.Region;
-import esdp.ktcallcenter.domain.shift.Shift;
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -25,8 +23,8 @@ public class EmployeeDTO {
     @NotBlank
     private String lastName;
 
-    private List<Shift> shifts = new ArrayList<>();
-    private List<Language> languages= new ArrayList<>();
+    private Set<Contract> contracts = new HashSet<>();
+    private Set<Language> languages= new HashSet<>();
     private Region region;
 
 static EmployeeDTO from(Employee employee){
@@ -34,7 +32,7 @@ static EmployeeDTO from(Employee employee){
             .id(employee.getId())
             .firstName(employee.getFirstName())
             .lastName(employee.getLastName())
-            .shifts(employee.getShifts())
+            .contracts(employee.getContracts())
             .languages(employee.getLanguages())
             .region(employee.getRegion())
             .build();
